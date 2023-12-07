b2af15a249a5f3260991168943395aac0465585a
valesquiveel <amandallmoreira@gmail.com>
2023-12-07 17:53:18 -0300

    v16
    Desenvolvimento pagina Loja

---
+++ js/script1.js
@@ -0,0 +1,14 @@
+
+// JAVASCRIPT
+
+function abrirOpcoes(){
+    if(opcoesMobile.style.display=='none'){
+        opcoesMobile.style.display='flex'
+            //Se o menu mobile estiver escondido então mostrar o Icone;/ = atribuição, ==comparação
+    }else{
+        opcoesMobile.style.display='none'
+            //Caso contrário não mostre o Icone.
+    }
+}
+    
+
