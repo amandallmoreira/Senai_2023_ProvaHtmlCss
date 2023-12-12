1fac16080e1deb4d1d48c8c537cc49d84c9defbe
Amanda Moreira <amandallmoreira@gmail.com>
2023-12-12 01:15:31 -0300

    v22
    Formatação de código

--- js/script1.js
+++ js/script1.js
@@ -2,43 +2,42 @@
 // JAVASCRIPT
 
 // menu HAMBURGUER
-function abrirOpcoes(){
-    if(opcoesMobile.style.display=='none'){
-        opcoesMobile.style.display='flex'
-            //Se o menu mobile estiver escondido então mostrar o Icone;/ = atribuição, ==comparação
-    }else{
-        opcoesMobile.style.display='none'
-            //Caso contrário não mostre o Icone.
+function abrirOpcoes() {
+    if (opcoesMobile.style.display == 'none') {
+        opcoesMobile.style.display = 'flex'
+        //Se o menu mobile estiver escondido então mostrar o Icone;/ = atribuição, ==comparação
+    } else {
+        opcoesMobile.style.display = 'none'
+        //Caso contrário não mostre o Icone.
     }
 }
 
 // VALIDAÇÃO DE LOGIN
-document.getElementById("botaoLogin").addEventListener("click",validarUser);
-    
-function validarUser(){
+document.getElementById("botaoLogin").addEventListener("click", validarUser);
+
+function validarUser() {
     var nomeUser = document.getElementById('nomeUser')
     var senhaUser = document.getElementById('senhaUser')
 
-    if(nomeUser.value.length == 0 && senhaUser.value.length == 0){
+    if (nomeUser.value.length == 0 && senhaUser.value.length == 0) {
         alert(`[ERRO] Verifique os dados e tente novamente.`)
         // Se o nomeUser e a senhaUser estiverem vazios, aparecerá mensagem de ERRO
     }
-    else{
+    else {
         alert('Parabéns, login realizado com sucesso.')
         document.write(`USUÁRIO: ${nomeUser.value} <br> SENHA: ${senhaUser.value}`)
-         //Qualquer valor no campo usuario e senha será validado
+        //Qualquer valor no campo usuario e senha será validado
     }
 }
 
 
 
 
-   
-
-    
 
 
 
 
 
-  
+
+
+
--- js/script2.js
+++ js/script2.js
@@ -1,9 +1,9 @@
 // CADASTRO DE USUARIO
-document.getElementById("btnEnviar").addEventListener("click",cadastroUser);
-var cadastro =[];
+document.getElementById("btnEnviar").addEventListener("click", cadastroUser);
+var cadastro = [];
 
-    
-function cadastroUser(){
+
+function cadastroUser() {
     let nome = document.querySelector('#nome').value;
     let sobrenome = document.querySelector('#sobrenome').value;
     let email = document.querySelector('#email').value;
@@ -17,8 +17,8 @@ function cadastroUser(){
     let date = document.querySelector('#date').value;
 
 
-    cadastro.push(nome,sobrenome,email,senha,senhanov,telefone,endereco,CEP,estado,genero,date);
+    cadastro.push(nome, sobrenome, email, senha, senhanov, telefone, endereco, CEP, estado, genero, date);
 
-    alert("Cadastro efetuado com sucesso"+cadastro[0])
+    alert("Cadastro efetuado com sucesso" + cadastro[0])
 
 }
